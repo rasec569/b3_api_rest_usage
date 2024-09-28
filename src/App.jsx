501ce0c66c4fde2b9974d7b0dd3052fee0b547f2
Cesar Augusto Valencia Valezques <cesaraugusto_569@hotmail.com>
2024-09-27 21:15:06 -0500

    coffee rest api with axios

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { AppCoffeeCarruselBasic } from './Components/AppCoffeeCarruselBasic'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       
+        <AppCoffeeCarruselBasic />
+      </div>
+    </>
+  )
+}
+
+export default App
